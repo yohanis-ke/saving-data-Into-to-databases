fad873596296de9f40c0c15d1bb46cb98e55ad8f
yohanis-ke <yohanes.ke@gmail.com>
2018-07-31 16:58:00 -0400

    Finishing Creating a Contact

--- contact.rb
+++ contact.rb
@@ -7,8 +7,13 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlit
 
 class Contact < ActiveRecord::Base
 
+  field :first_name, as: :string
+    field :last_name,  as: :string
+    field :email,      as: :string
+    field :note,       as: :text
+
   def full_name
-   "#{@first_name} #{@last_name}"
+   puts "#{@first_name} #{@last_name}"
   end
 
 end
--- crm.rb
+++ crm.rb
@@ -49,9 +49,16 @@ class CRM
     print 'Enter a Note: '
     note = gets.chomp
 
-    Contact.create(first_name, last_name, email, note)
-  end
+    # Contact.create(first_name, last_name, email, note)
 
+
+  contact = Contact.create(
+        first_name: first_name,
+        last_name:  last_name,
+        email:      email,
+          note:     note
+                          )
+    end
   def modify_existing_contact
     contact = find_contact
 
